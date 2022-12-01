bddd172928a8aad19bffc55916ee5c2ed624acdf
jasonadidela <jasonadidela@gmail.com>
2022-12-01 21:40:04 +0530

    Given Max Number at 1st Position return the Same Number.

--- TestCase.java
+++ TestCase.java
@@ -1,5 +1,7 @@
 public class TestCase {
     public static void main(String[] args) {
-        System.out.println("Welcome to Test case branch");
+        int x1 = 3, y1 = 4, z1 = 5;
+
+        new intMaximumTest(x1, y1, z1).maximum();
     }
 }
