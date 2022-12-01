224d0569ef7a13312c2163d051e6c6670e452391
jasonadidela <jasonadidela@gmail.com>
2022-12-01 23:13:10 +0530

    Final code is added to MaximumTest

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,5 +1,51 @@
-public class MaximumTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Maximum Test in Refactor 2 Branch");
+public class MaximumTest<T extends Comparable<T>> {
+
+    T x, y, z;
+
+    public MaximumTest(T x, T y, T z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public T maximum(){
+        return MaximumTest.maximum(x, y, z);
+    }
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z){
+        T max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        printMax(x, y, z, max);
+        return max;
+    }
+
+    public static String testMaximum(String x, String y, String z){
+        String max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        printMax(x, y, z, max);
+        return max;
+    }
+    public static <T> void printMax(T x, T y, T z, T max){
+
+        System.out.printf("Max of %s, %s and %s is %s\n", x, y, z, max);
+    }
+    public static void main(String[] args){
+        Integer xInt = 3, yInt = 4, zInt = 5;
+        Float xFl = 6.6f, yFl = 8.8f, zFl = 7.7f;
+        String xStr = "Orange", yStr = "Apple", zStr = "Pear";
+
+		MaximumTest.testMaximum(xStr, yStr, zStr);
+        new MaximumTest(xInt, yInt, zInt).maximum();
+        new MaximumTest(xFl, yFl, zFl).maximum();
+        new MaximumTest(xStr, yStr, zStr).maximum();
     }
 }
