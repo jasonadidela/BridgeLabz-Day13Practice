4eb9a5c235b283845cf9476fb999519cdbfbc678
jasonadidela <jasonadidela@gmail.com>
2022-12-01 21:45:51 +0530

    Given Max Number at 3rd Position return the Same Number.

--- TestCase.java
+++ TestCase.java
@@ -2,8 +2,10 @@ public class TestCase {
     public static void main(String[] args) {
         int x1 = 3, y1 = 4, z1 = 5;
         int x2 = 6, y2 = 10, z2 = 15;
+        int x3 = 100, y3 = 400, z3 = 50;
 
         new intMaximumTest(x1, y1, z1).maximum();
         new intMaximumTest(x2, y2, z2).maximum();
+        new intMaximumTest(x3, y3, z3).maximum();
     }
 }
