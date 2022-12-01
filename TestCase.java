165b539911108213853c29258b51225d8c62d5ef
jasonadidela <jasonadidela@gmail.com>
2022-12-01 21:43:47 +0530

    Given Max Number at 2st Position return the Same Number.

--- TestCase.java
+++ TestCase.java
@@ -1,7 +1,9 @@
 public class TestCase {
     public static void main(String[] args) {
         int x1 = 3, y1 = 4, z1 = 5;
+        int x2 = 6, y2 = 10, z2 = 15;
 
         new intMaximumTest(x1, y1, z1).maximum();
+        new intMaximumTest(x2, y2, z2).maximum();
     }
 }
